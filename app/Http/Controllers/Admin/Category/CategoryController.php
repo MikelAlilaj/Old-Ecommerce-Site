1e20b6f659a0d68ee33d54b1a0885b752987a7d9
MikelAlilaj <alilajmikel@gmail.com>
2020-04-26 01:17:38 +0200

    Category :  New, Edit, Update, Delete works 100%

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Admin\Category;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -12,6 +14,67 @@ class CategoryController extends Controller
         $this->middleware('auth:admin');
     }
 
+    public function category(){
 
+        $category=Category::all();
+        return view('admin.category.category', compact('category'));
+
+    }
+
+    public function storecategory(Request $request){
+
+        $validateData = $request->validate([
+            'category_name'=>'required|unique:categories|max:255',
+            ]);
+
+
+        $category=new Category();
+        $category->category_name=$request->category_name;
+        $category->save();
+
+        $notification=array(
+            'messege'=>'Category Added Successfully!',
+            'alert-type'=>'success'
+        );
+        return Redirect()->back()->with($notification);
+    }
+
+    public function Deletecategory($id){
+        DB::table('categories')->where('id',$id)->delete();
+        $notification=array(
+            'messege'=>'Category Deleted Successfully!',
+            'alert-type'=>'success'
+        );
+        return Redirect()->back()->with($notification);
+    }
+
+    public function Editcategory($id){
+        $category = DB::table('categories')->where('id',$id)->first();
+        return view('admin.category.edit_category',compact('category'));
+    }
+
+    public function Updatecategory(Request $request,$id){
+        $validateData = $request->validate([
+            'category_name'=>'required|max:255',
+        ]);
+
+        $data=array();
+        $data['category_name']=$request->category_name;
+        $update=DB::table('categories')->where('id',$id)->update($data);
+        if ($update){
+            $notification=array(
+                'messege'=>'Category Update Successfully!',
+                'alert-type'=>'success'
+            );
+            return Redirect()->route('categories')->with($notification);
+
+        }else {
+            $notification=array(
+                'messege'=>'Nothing to Update',
+                'alert-type'=>'error'
+            );
+            return Redirect()->route('categories')->with($notification);
+        }
+    }
 
 }
